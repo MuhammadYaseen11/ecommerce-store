6fafcd2e5bed596ddac9dd80ca05149c42f37d1f
YaseenStu <111099331+MuhammadYaseen11@users.noreply.github.com>
2024-07-04 15:21:23 +0000

    Logo added.
    Box and AppBar is styled using `styled`
    
    const StyledHeader = styled(AppBar)`
        background: #2874f0;
        height: 55px;
    `
    
    const StyledBox = styled(Box)`
        margin-left: 3%;
    `

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,29 @@
 
+//images
+import Logo from "/ecommerce-store/client/src/images/header/seller.png"
+
+
+import {AppBar, Toolbar, Box, styled} from '@mui/material';
+
+const StyledHeader = styled(AppBar)`
+    background: #2874f0;
+    height: 55px;
+`
+
+const StyledBox = styled(Box)`
+    margin-left: 3%;
+`
+
 const Header = () => {
     return (
-        <div>
-            Hello form header
-        </div>
+        <StyledHeader>
+            <Toolbar>
+                <StyledBox>
+                    <img src={Logo} alt="Logo" style={{width: 45}}/>
+                </StyledBox>
+
+            </Toolbar>
+        </StyledHeader>
     )
 }
 
