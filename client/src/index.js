cda831202570698088dd3aeec4ca3bb316915536
YaseenStu <111099331+MuhammadYaseen11@users.noreply.github.com>
2024-07-04 14:58:49 +0000

    mui installed
    material ui is used to utilize pre-existed code in your code. we will use this to save time.
    
    npm install @mui/material @emotion/react @emotion/styled

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-//components
-import Header from './components/Header/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
+    <App />
   </React.StrictMode>
 );
 
