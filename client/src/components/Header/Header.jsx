46d0e32ad8022b1b609070bab6a44d952d8492ae
YaseenStu <111099331+MuhammadYaseen11@users.noreply.github.com>
2024-07-05 15:01:19 +0000

    HTML Style info
    here we cant directly style html element(img). Because the its not the part of materuial ui, hence it will treat as a variable.
    
    we need to pass this in string like this 'img'.

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@
 import Logo from "/ecommerce-store/client/src/images/header/seller.png"
 
 
-import {AppBar, Toolbar, Box, styled} from '@mui/material';
+import {AppBar, Toolbar, Box, Typography, styled} from '@mui/material';
 
 const StyledHeader = styled(AppBar)`
     background: #2874f0;
@@ -11,15 +11,31 @@ const StyledHeader = styled(AppBar)`
 `
 
 const StyledBox = styled(Box)`
-    margin-left: 3%;
+    margin-left: 12%;
+    line-height: 0;
 `
+const SubHeading = styled(Typography)`
+    font-size: 10px;
+    font-style: italic;
+`
+const PlusImage = styled(img)
 
 const Header = () => {
+
+    const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
+    const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png';
     return (
         <StyledHeader>
             <Toolbar>
                 <StyledBox>
-                    <img src={Logo} alt="Logo" style={{width: 45}}/>
+                    <img src={logoURL} alt="Logo" style={{width: 75}}/>
+                    <Box>
+                        <SubHeading>Explore 
+                            <Box component="span" style={{color: '#FFE500'}}> Plus
+                            </Box>
+                            </SubHeading>
+                            <img src={subURL} />
+                    </Box>
                 </StyledBox>
 
             </Toolbar>
