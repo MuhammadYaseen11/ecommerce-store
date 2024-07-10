4962e09664eb68273604f89b40366091dd474a49
YaseenStu <111099331+MuhammadYaseen11@users.noreply.github.com>
2024-07-10 13:33:50 +0000

    npm i react-multi-carousel
    react carousel installed to scroll NavBar.jsx

--- client/src/components/Header/CustomButtons.jsx
+++ client/src/components/Header/CustomButtons.jsx
@@ -1,13 +1,38 @@
+import { Box, Button, Typography, styled } from "@mui/material";
+import ShoppingCart from '@mui/icons-material/ShoppingCart';
+
+const Wrapper = styled(Box)`
+  display: flex;
+  margin: 0 3% 0 auto;
+
+  & > * {
+    margin-right: 40px;
+    font-size: 14px;
+    font-weight: bold;
+    color: white;
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const LoginButton = styled(Button)`
+    color: '#2874f0',
+    background: '#FFFFFF',
+`;
 
 
+const CustomButtons = () => {
+  return (
+    <Wrapper>
+      <LoginButton variant="contained">Login</LoginButton>
+      <Typography style={{ marginTop: 3, width: 135 }}>Become a Seller</Typography>
+      <Typography style={{ marginTop: 3 }}>More</Typography>
+      <Box style={{ display: 'flex', alignItems: 'center' }}>
+        <ShoppingCart style={{ marginRight: 5 }} />
+        <Typography>Cart</Typography>
+      </Box>
+    </Wrapper>
+  );
+};
 
-const SearchButtons = () => {
-    return (
-        <div> Login</div>
-    )
-}
-
-
-
-
-export default SearchButtons;
+export default CustomButtons;
--- client/src/components/Header/Search.jsx
+++ client/src/components/Header/Search.jsx
@@ -1,40 +1,40 @@
-
-
-import {Box, InputBase, styled } from "@mui/material"; 
-
-import SearchIcon from "@mui/icons-material/Search"
-
+import { Box, InputBase, styled } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 const SearchContainer = styled(Box)`
-    background: #fff;
-    width: 38%;
-    border-radius: 2px;
-    margin-left: 10px;
-    display: flex;
+  background: #fff;
+  width: 30%; /* Adjusted width */
+  border-radius: 2px;
+  margin-left: 10px;
+  display: flex;
+  align-items: center; /* Center vertically */
+  height: 40px; /* Adjust height to match the desired look */
 `;
 
 const InputSearchBase = styled(InputBase)`
-padding-left: 20px;
-width: 100%; 
-font-size: unset; 
+  padding-left: 10px; /* Adjusted padding */
+  width: 100%;
+  font-size: 14px; /* Adjusted font size */
 `;
 
 const SearchIconWrapper = styled(Box)`
-color: Blue;
-padding: 5px;
-`//padding: 5px;  
+  color: blue;
+  padding: 5px;
+  display: flex;
+  align-items: center; /* Center icon vertically */
+`;
 
 const Search = () => {
-    return (
-        <SearchContainer>
-        <InputSearchBase style={{minHeight: 55}} 
-        placeholder='Search for Products, Brands and more'
-        />
-        <SearchIconWrapper>
-            <SearchIcon />
-        </SearchIconWrapper>
-        </SearchContainer>
-    );
-}
+  return (
+    <SearchContainer>
+      <InputSearchBase
+        placeholder="Search for Products, Brands and more"
+      />
+      <SearchIconWrapper>
+        <SearchIcon />
+      </SearchIconWrapper>
+    </SearchContainer>
+  );
+};
 
 export default Search;
