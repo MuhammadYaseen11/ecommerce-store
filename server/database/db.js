97e2b70639552844408b48c2792c71ecd1c9d05c
YaseenStu <111099331+MuhammadYaseen11@users.noreply.github.com>
2024-07-10 17:07:44 +0000

    products added to database
    using id unique to stop adding the same data with same id.

--- server/database/db.js
+++ server/database/db.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 
 
 
-export const Connection = async () => {
-    const URL = 'mongodb+srv://ecommerceweb:ecommerce123@ecommerce-web.zbkp3ij.mongodb.net/?retryWrites=true&w=majority&appName=ecommerce-web';
+export const Connection = async (username, password) => {
+    const URL = `mongodb+srv://${username}:${password}@ecommerce-web.zbkp3ij.mongodb.net/?retryWrites=true&w=majority&appName=ecommerce-web`;
     try {
        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true});
        console.log('Database connected successfully');
--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 import express from 'express'; //no need to use (const express = require('express') this is old version)
-
+import dotenv from 'dotenv';
 
 import Connection from './database/db.js';
-
+import DefaultData from './default.js';
 
 const app = express();
 
+dotenv.config();
 
 //we use listen to create express server.
 //takes two aurgumnet 1, port 2. callback function
@@ -13,7 +14,14 @@ const app = express();
 //first aurgument
 const PORT = 8000;
 
-Connection();
+
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+Connection(USERNAME, PASSWORD);
 
 //second aurgument within applisten
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+
+//call default function after creating server Connection();
+
+DefaultData();
