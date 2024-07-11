e7a59b82b5f80e5c2f29e969119536d9e649bb22
YaseenStu <111099331+MuhammadYaseen11@users.noreply.github.com>
2024-07-11 14:30:32 +0000

    deleteOne(Product8) is deleted
    also connected with Usman database

--- server/database/db.js
+++ server/database/db.js
@@ -4,8 +4,10 @@ import mongoose from "mongoose";
 
 
 export const Connection = async (username, password) => {
-    const URL = `mongodb+srv://${username}:${password}@ecommerce-web.zbkp3ij.mongodb.net/?retryWrites=true&w=majority&appName=ecommerce-web`;
-    try {
+    //const URL = `mongodb+srv://${username}:${password}@ecommerce-web.zbkp3ij.mongodb.net/?retryWrites=true&w=majority&appName=ecommerce-web`;
+   // const URL = `mongodb+srv://${username}:${password}@ecommercewebapp.czzthsn.mongodb.net/?retryWrites=true&w=majority&appName=ecommercewebapp`;
+      const URL = `mongodb+srv://${username}:${password}@ecommerce-web.zbkp3ij.mongodb.net/?retryWrites=true&w=majority&appName=ecommerce-web`;
+   try {
        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true});
        console.log('Database connected successfully');
 
--- server/default.js
+++ server/default.js
@@ -3,11 +3,19 @@ import { products } from "./constants/data.js"
 import Product from "./model/ProductSchema.js";
 
 
-
+// Function to delete a product by ID
+const deleteProductById = async (id) => {
+    try {
+        await Product.deleteOne({ id: id });
+        console.log(`Product with ID ${id} deleted successfully`);
+    } catch (error) {
+        console.error('Error while deleting product:', error.message);
+    }
+};
 
 const DefaultData = async () => {
     try{
-        await Product.insertMany(products);
+      //  await Product.insertMany(products);
 
         console.log('Data imported successfully');
     } catch(error) {
@@ -15,5 +23,11 @@ const DefaultData = async () => {
     }
 
 }
+
+// Call the function to delete the product
+deleteProductById('product8').then(() => {
+    // After deletion, insert the default data
+    DefaultData();
+});
 
 export default DefaultData;
