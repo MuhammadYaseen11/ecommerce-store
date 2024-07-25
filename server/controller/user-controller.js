da6ee576506e974c242064489df86b2e2aef45a8
YaseenStu <111099331+MuhammadYaseen11@users.noreply.github.com>
2024-07-25 15:32:30 +0000

    npm i body-parser installed
    using npm i body-parser. used to handle PostAPIs.

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,3 +1,5 @@
+
+
 export const userSignUp = (request, response) => {
     try {
         console.log(request.body);
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,8 +1,9 @@
 import express from  'express';
+import { userSignUp } from '../controller/user-controller.js';
 
 const router = express.Router();
 
-router.post('/signup');
+router.post('/signup', userSignUp);
 
 
 export default router;
