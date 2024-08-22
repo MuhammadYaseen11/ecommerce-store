69b43de90494af2f4d91c7eca5420b4bd87434ba
YaseenStu <111099331+MuhammadYaseen11@users.noreply.github.com>
2024-08-22 16:45:30 +0000

    PACKAGES INSTALLED
    npm install react-countdown
    
    npm install react-router-dom

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -4,7 +4,9 @@ import { Box, styled } from '@mui/material';
 
 import NavBar from '../home/NavBar';
 import Banner from '../home/Banner';
-
+import MidSlide from '../home/MidSlide';
+import MidSection from '../home/MidSection';
+import Slide from '../home/Slide';
 import { useSelector, useDispatch } from 'react-redux'; // hooks. useSelector is custom hook.
 import { getProducts as listProducts } from '../../redux/actions/product-actions';
 
@@ -23,11 +25,38 @@ const Home = () => {
     useEffect(() => {
         dispatch(listProducts())
     }, [dispatch])
+
     return (
         <>
             <NavBar />
             <Component>
-            <Banner />
+                <Banner />
+                <MidSlide products={products} />
+                <MidSection />
+                <Slide
+                    data={products} 
+                    title='Discounts for You'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={products} 
+                    title='Suggested Items'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={products} 
+                    title='Top Selection'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={products} 
+                    title='Recommended Items'
+                    timer={false} 
+                    multi={true} 
+                />
             </Component>
         </>
     )
